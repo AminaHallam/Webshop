d2d6fda1361a7817988e8a467698e93aa18b0394
Amina Hallam <amina.hallam@gmail.com>
2022-02-10 11:27:47 +0100

    Add-products-and-categories-into-webpage

--- api/classes/createInstanceFunctions.php
+++ api/classes/createInstanceFunctions.php
@@ -2,8 +2,8 @@
 
 include_once("./../classes/product.php");
 include_once("./../classes/category.php");
-include_once("./../classes/order.php");
-include_once("./../classes/user.php");
+//include_once("./../classes/order.php");
+//include_once("./../classes/user.php");
 
 /* FETCH_FUNC tar alla kolumner från den här funktionen och ger till varje specifik instans vi får från databasen. Här är det viktigt att de speglar kolumnerna som vi faktiskt får ut från databasen  */
 function createProduct($productId, $name, $description, $unitPrice, $unitsInStock, $image) { /* ändrade id till productId */
--- logic.js
+++ logic.js
@@ -1,54 +1,54 @@
 
-
 function onLoad() {
-    getAllProducts()
-    getProductFromId(1)
-    getAllCategories()
-    getCategoryFromId(3)
+    
+    getAllCategories() 
+    
+    
 }
 
 document.getElementById("menu").addEventListener("click", openMenu);
 
- 
-function openMenu() {
+// document.querySelector("#all").addEventListener("click", getAllProducts); 
+
+
     
-    document.getElementById("dropdown").classList.toggle("active");
-}
-
-
-
-async function makeRequest(url, method, body) {
-    try {
-        let response = await fetch(url, {
-            method,
-            body
-        })
-        let result = await response.json();
-
-        return result
-    } catch(err) {
-        console.error(err)
+    function openMenu() {
+        
+        document.getElementById("dropdown").classList.toggle("active");
+        
     }
-}
-
-
-// functions for product
-async function getAllProducts() {
-    const action = "getAll";
-
-    let allProducts = await makeRequest(`./api/receivers/productReceiver.php?action=${action}`, "GET")
-    console.log(allProducts)
-}
-
-
-async function getProductFromId(id) {
-    const action = 'getById'; 
+    
+    
+    
+    async function makeRequest(url, method, body) {
+        try {
+            let response = await fetch(url, {
+                method,
+                body
+            })
+            let result = await response.json();
+            
+            return result
+        } catch(err) {
+            console.error(err)
+        }
+    }
     
 
-    let specificProduct = await makeRequest(`./../api/receivers/productReceiver.php?action=${action}&id=${id}`, "GET")
-    console.log(specificProduct)
-}
+    
+    
+    
+    
+  /*   async function getProductFromId(id) {
+        const action = 'getById'; 
+        
+        let specificProduct = await makeRequest(`./../api/receivers/productReceiver.php?action=${action}&id=${id}`, "GET")
+        // console.log(specificProduct)
+        console.log(specificProduct)
 
+       
+    }
+     */
 
 
 
@@ -59,18 +59,47 @@ async function getProductFromId(id) {
 async function getAllCategories() {
     const action = "getAll";
     let allCategories = await makeRequest(`./../api/receivers/categoryReceiver.php?action=${action}`, "GET")
-    console.log(allCategories); 
+    
+    
+    for (let i = 0; i < allCategories.length; i++) {
+        const element = allCategories[i];
+        
+        const ul = document.getElementById("dropdown");
+        let productContainer = document.createElement("div")
+        productContainer.classList.add("productContainer")
+        let title = document.createElement("a")
+        title.href = 'collectionPage.html?id=' + element.categoryId
+        title.innerHTML = element.categoryName; 
+        ul.append(productContainer)
+        productContainer.append(title)
+        
+        
+    }  
+    
+    
 }
 
 
-/* Hämtar alla produkter som tillhör en specifik kategori */
-async function getCategoryFromId(idToGet) {
-    const action = "getById";
-    let specificCategory = await makeRequest(`./../api/receivers/categoryReceiver.php?action=${action}&id=${idToGet}`, "GET")
-    console.log(specificCategory) 
+
+/* async function renderCategory(list){
+    for (let i = 0; i < list.length; i++) {
+        
+        const element = list[i];
+        const main = document.getElementsByTagName("main")[0];
+        let productContainer = document.createElement("div")
+        productContainer.classList.add("productContainer")
+        let title = document.createElement("h2")
+        title.innerHTML = element.categoryName;  
+        main.append(productContainer)
+        productContainer.append(title)
+
+    
 }
 
+} */
+    
 
 
 
-window.addEventListener('load', onLoad);
+
+window.addEventListener('load', onLoad)
