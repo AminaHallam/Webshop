650ebd7df42acf32d1bf03ae01e71aab19b5dae7
Angelica Moberg <angelica.moberg@hotmail.com>
2022-02-12 10:20:08 +0100

    Inlogged user displayed on website

--- helperFunctions/fetchHelper.js
+++ helperFunctions/fetchHelper.js
@@ -23,49 +23,56 @@ export async function makeRequest(url, method, body) {
 export async function verifyAdmin() {
     const action = 'verifyAdmin'; 
     let verifyA = await makeRequest(`./../api/receivers/userReceiver.php?action=${action}`, "GET")
-    /* console.log(verifyA) */
     return verifyA    
 }
 
 // Visar den rätta layouten beroende på om det är en kund eller admin som är inloggad. Om ingen är inloggad behålls den befintliga layouten.
 export async function showCorrectLayout() {
 
+    /* Checkar om någon användare finns i session(dvs inloggad) */
     let checkIfInlogged = await getUser();
-
     if(!checkIfInlogged) {
         document.querySelector(".adminIcons").classList.add("none");
-        console.log("Ingen är inloggad")
+        loggedInUser.classList.add("none")
         return
     }
 
-    let checkAdmin = await verifyAdmin();
 
+    /* Visar vem som är inloggad */
+    const loggedInUser = document.querySelector(".loggedInUser")
+    loggedInUser.classList.remove("none")
+    let activeUser = document.createElement("p")
+    activeUser.innerText = "Logged in user: " + checkIfInlogged.FirstName
+    loggedInUser.append(activeUser)
+
+
+
+    /* Checkar om den inloggade användaren är admin eller inte. Här påverkar vi layouten i headern*/
+    let checkAdmin = await verifyAdmin();
      if(checkAdmin) {
         console.log("Du är admin");
-
         document.querySelector(".adminIcons").classList.remove("none");
         document.querySelector(".loginIcon").classList.add("none")
 
     } else {
         console.log("Du är en vanlig kund");
-        
         document.querySelector(".adminIcons").classList.remove("none");
         document.querySelector(".adminSetting").classList.add("none");
         document.querySelector(".loginIcon").classList.add("none")
     } 
 }
 
-// Hämta användarinfo från SESSION vid inloggad användare.
+
+// Hämtar Id, förnamn och efternamn från SESSION vid inloggad användare. Vid ej inloggad användare så returnerar getUser false.
 export async function getUser() {
     const action = 'getUser'; 
     let getUser = await makeRequest(`./../api/receivers/userReceiver.php?action=${action}`, "GET")
-     /* console.log(getUser)  */
-     return getUser
+    return getUser
 }
 
 
+// Loggar ut användare genom att göra en session_destroy i PHP.
 export async function logOut(){
-
     const action = "destroySession"
     let logoutUser = await makeRequest(`./../api/receivers/userReceiver.php?action=${action}`, "GET")
     alert("You are out!")    
@@ -75,7 +82,7 @@ export async function logOut(){
 
 /* Order */
 
-/* Hämtar alla ordrar */
+// Hämtar alla ordrar
 export async function getAllOrders() {
     const action = "getAll";
     let allOrders = await makeRequest(`./../api/receivers/orderReceiver.php?action=${action}`, "GET")
@@ -83,14 +90,14 @@ export async function getAllOrders() {
 }
 
 
-/* Hämtar en order baserat på ett orderid */
+// Hämtar en order baserat på ett orderid
 export async function getOrderById(idToGet) {
     const action = "getById";
     let specificOrder = await makeRequest(`./../api/receivers/orderReceiver.php?action=${action}&id=${idToGet}`, "GET")
     console.log(specificOrder) 
 }
 
-/* Hämtar ordrar baserat på statusid eller userid */
+// Hämtar ordrar baserat på statusid eller userid 
 export async function getOrdersByOtherId(idToGet, type) {
     const action = "getByOtherId";
     let specificOther = await makeRequest(`./../api/receivers/orderReceiver.php?action=${action}&id=${idToGet}&type=${type}`, "GET")
@@ -105,7 +112,7 @@ export async function getOrdersByOtherId(idToGet, type) {
 
 /* Product */
 
-/* Hämtar alla produkter */
+// Hämtar alla produkter 
 export async function getAllProducts() {
     const action = "getAll";
 
@@ -114,7 +121,7 @@ export async function getAllProducts() {
 }
 
 
-/* Hämtar en produkt baserat på ett produktid */
+// Hämtar en produkt baserat på ett produktid 
 export async function getProductFromId(id) {
 const action = 'getById'; 
 
