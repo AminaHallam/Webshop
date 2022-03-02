c3af3b48da77210fc54bb5159cb4b6a1c6ba84d5
Amina Hallam <amina.hallam@gmail.com>
2022-03-02 16:26:33 +0100

    Worked-with-HeaderPicture-and-CartIcon

--- js/logic.js
+++ js/logic.js
@@ -72,39 +72,5 @@ async function addSubscriptionNews(e) {
 
 
 
-
-/* Kolla om man fortsätta på funktionen med att slida vidare  */
-
-const slider = document.querySelector('.gallery');
-let isDown = false;
-let startX;
-let scrollLeft;
-
-slider.addEventListener('mousedown', e => {
-  isDown = true;
-  slider.classList.add('active');
-  startX = e.pageX - slider.offsetLeft;
-  scrollLeft = slider.scrollLeft;
-});
-slider.addEventListener('mouseleave', _ => {
-  isDown = false;
-  slider.classList.remove('active');
-});
-slider.addEventListener('mouseup', _ => {
-  isDown = false;
-  slider.classList.remove('active');
-});
-slider.addEventListener('mousemove', e => {
-  if (!isDown) return;
-  e.preventDefault();
-  const x = e.pageX - slider.offsetLeft;
-  const SCROLL_SPEED = 3;
-  const walk = (x - startX) * SCROLL_SPEED;
-  slider.scrollLeft = scrollLeft - walk;
-});
-
-
-
-
 window.addEventListener('load', onLoad)
 
